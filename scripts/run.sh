8302971dc0517490edd52b18a13e9975dbed3b81
Maxim Samoilov <nitive@icloud.com>
2021-02-18 12:18:06 +0300

    Temporary allow to use DENO_IMPORT_MAP env to use deno import maps
    TODO: use flag instead

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -eu
 
-$HELM_PLUGIN_DIR/bin/deno run --unstable --allow-net --allow-read --allow-write --allow-run --allow-env --quiet $HELM_PLUGIN_DIR/bin/bundle.js $@
+# Temporary allow to use DENO_IMPORT_MAP env to use deno import maps
+# TODO: use flag instead
+if [ ! -z "${DENO_IMPORT_MAP:-}" ]; then
+  importmap="--importmap=$DENO_IMPORT_MAP"
+fi
+
+$HELM_PLUGIN_DIR/bin/deno run --unstable --allow-net --allow-read --allow-write --allow-run --allow-env --quiet ${importmap:-} $HELM_PLUGIN_DIR/bin/bundle.js $@
